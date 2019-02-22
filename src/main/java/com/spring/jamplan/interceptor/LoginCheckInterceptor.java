c9d28d88ec903dfab4d15aeb6280015ea63aa655
YJIEUN <youje95@naver.com>
2019-02-22 14:58:56 +0900

    Revert "Final JamPlanner commit"
    This reverts commit c3f7145bdb214bd447c4a2072814a6d25fcbc45e.

--- src/main/java/com/spring/jamplan/interceptor/LoginCheckInterceptor.java
+++
@@ -1,7 +0,0 @@
-package com.spring.jamplan.interceptor;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class LoginCheckInterceptor extends HandlerInterceptorAdapter{
-
-}
--- src/main/java/com/spring/jamplan/manageplan/ManagePlanMapper.java
+++
@@ -1,9 +0,0 @@
-package com.spring.jamplan.manageplan;
-
-import java.util.ArrayList;
-
-import com.spring.jamplan.model.TeamInfoVO;
-
-public interface ManagePlanMapper {
-	public ArrayList<TeamInfoVO> getOthers(TeamInfoVO teamInfo);
-}
